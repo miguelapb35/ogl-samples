17ebf4d7ac013c14fa8834c0eb17ea421e6bf02f
Christophe Riccio <mail@g-truc.net>
2012-09-17 13:28:10 +0200

    Fixed multisample-position sample

--- samples/gl-420-fbo-multisample-position-amd.cpp
+++ samples/gl-420-fbo-multisample-position-amd.cpp
@@ -14,8 +14,8 @@
 namespace
 {
 	std::string const SAMPLE_NAME("OpenGL Sample Position");
-	std::string const SHADER_VERT_SOURCE(glf::DATA_DIRECTORY + "ogl-420/multisample-amd.vert");
-	std::string const SHADER_FRAG_SOURCE(glf::DATA_DIRECTORY + "ogl-420/multisample-amd.frag");
+	std::string const SHADER_VERT_SOURCE(glf::DATA_DIRECTORY + "gl-420/multisample-amd.vert");
+	std::string const SHADER_FRAG_SOURCE(glf::DATA_DIRECTORY + "gl-420/multisample-amd.frag");
 	std::string const TEXTURE_DIFFUSE(glf::DATA_DIRECTORY + "kueken3-bgr8.dds");
 	glm::ivec2 const FRAMEBUFFER_SIZE(320, 240);
 	int const SAMPLE_SIZE_WIDTH(640);
