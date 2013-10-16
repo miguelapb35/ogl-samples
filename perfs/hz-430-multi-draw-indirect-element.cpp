621c219e75b1cbb8ff64c2e010fe0627d0eedd4c
Christophe Riccio <mail@g-truc.net>
2013-10-17 01:40:53 +0200

    Added performance samples

---
+++ perfs/hz-430-multi-draw-indirect-element.cpp
@@ -0,0 +1,253 @@
+//**********************************
+// OpenGL Multi Draw Indirect
+// 25/07/2012 - 15/11/2012
+//**********************************
+// Christophe Riccio
+// ogl-samples@g-truc.net
+//**********************************
+// G-Truc Creation
+// www.g-truc.net
+//**********************************
+
+#include <glf/glf.hpp>
+
+namespace
+{
+	char const * SAMPLE_NAME("OpenGL Multi draw indirect");
+	char const * VERT_SHADER_SOURCE("hz-430/multi-draw-indirect.vert");
+	char const * FRAG_SHADER_SOURCE("hz-430/multi-draw-indirect.frag");
+	char const * TEXTURE_DIFFUSE("kueken1-bgr8.dds");
+	int const SAMPLE_SIZE_WIDTH(640);
+	int const SAMPLE_SIZE_HEIGHT(480);
+	int const SAMPLE_MAJOR_VERSION(4);
+	int const SAMPLE_MINOR_VERSION(2);
+
+	glf::window Window(glm::ivec2(SAMPLE_SIZE_WIDTH, SAMPLE_SIZE_HEIGHT));
+
+	GLsizei const ElementCount(6);
+	GLsizeiptr const ElementSize = ElementCount * sizeof(glm::uint32);
+	glm::uint32 const ElementData[ElementCount] =
+	{
+		0, 1, 2,
+		0, 2, 3
+	};
+
+	GLsizei const VertexCount(4);
+	GLsizeiptr const VertexSize = VertexCount * sizeof(glm::vec2);
+	glm::vec2 const VertexData[VertexCount] =
+	{
+		glm::vec2(-1.0f,-1.0f),
+		glm::vec2( 1.0f,-1.0f),
+		glm::vec2( 1.0f, 1.0f),
+		glm::vec2(-1.0f, 1.0f)
+	};
+
+	GLsizei const DrawCount(1000000);
+
+	namespace buffer
+	{
+		enum type
+		{
+			VERTEX,
+			ELEMENT,
+			TRANSFORM,
+			INDIRECT,
+			MAX
+		};
+	}//namespace buffer
+/*
+	struct DrawArraysIndirectCommand
+	{
+		GLuint count;
+		GLuint primCount;
+		GLuint first;
+		GLuint baseInstance;
+	};
+*/
+	struct DrawElementsIndirectCommand
+	{
+		GLuint count;
+		GLuint primCount;
+		GLuint firstIndex;
+		GLint  baseVertex;
+		GLuint baseInstance;
+	};
+
+	GLuint VertexArrayName(0);
+	GLuint PipelineName(0);
+	GLuint ProgramName(0);
+	GLuint BufferName[buffer::MAX];
+	GLuint QueryName(0);
+
+}//namespace
+
+bool initProgram()
+{
+	bool Validated(true);
+	
+	glGenProgramPipelines(1, &PipelineName);
+
+	glf::compiler Compiler;
+	GLuint VertShaderName = Compiler.create(GL_VERTEX_SHADER, glf::DATA_DIRECTORY + VERT_SHADER_SOURCE, 
+		"--version 420 --profile core");
+	GLuint FragShaderName = Compiler.create(GL_FRAGMENT_SHADER, glf::DATA_DIRECTORY + FRAG_SHADER_SOURCE,
+		"--version 420 --profile core");
+	Validated = Validated && Compiler.check();
+
+	ProgramName = glCreateProgram();
+	glProgramParameteri(ProgramName, GL_PROGRAM_SEPARABLE, GL_TRUE);
+	glAttachShader(ProgramName, VertShaderName);
+	glAttachShader(ProgramName, FragShaderName);
+	glLinkProgram(ProgramName);
+	Validated = Validated && glf::checkProgram(ProgramName);
+
+	if(Validated)
+		glUseProgramStages(PipelineName, GL_VERTEX_SHADER_BIT | GL_FRAGMENT_SHADER_BIT, ProgramName);
+
+	return Validated;
+}
+
+bool initBuffer()
+{
+	glGenBuffers(buffer::MAX, BufferName);
+
+	glBindBuffer(GL_ARRAY_BUFFER, BufferName[buffer::VERTEX]);
+	glBufferData(GL_ARRAY_BUFFER, VertexSize, VertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, BufferName[buffer::ELEMENT]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, ElementSize, ElementData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_UNIFORM_BUFFER, BufferName[buffer::TRANSFORM]);
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), NULL, GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+	std::vector<DrawElementsIndirectCommand> Commands;
+	Commands.resize(DrawCount);
+	for(std::size_t i = 0; i < Commands.size(); ++i)
+	{
+		Commands[i].count = ElementCount;
+		Commands[i].primCount = 2;
+		Commands[i].firstIndex = 0;
+		Commands[i].baseVertex = 0;
+		Commands[i].baseInstance = 0;
+	}
+
+	glBindBuffer(GL_DRAW_INDIRECT_BUFFER, BufferName[buffer::INDIRECT]);
+	glBufferData(GL_DRAW_INDIRECT_BUFFER, sizeof(DrawElementsIndirectCommand) * Commands.size(), &Commands[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_DRAW_INDIRECT_BUFFER, 0);
+
+	return true;
+}
+
+bool initVertexArray()
+{
+	glGenVertexArrays(1, &VertexArrayName);
+	glBindVertexArray(VertexArrayName);
+		glBindBuffer(GL_ARRAY_BUFFER, BufferName[buffer::VERTEX]);
+		glVertexAttribPointer(glf::semantic::attr::POSITION, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), GLF_BUFFER_OFFSET(0));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		glEnableVertexAttribArray(glf::semantic::attr::POSITION);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, BufferName[buffer::ELEMENT]); 
+	glBindVertexArray(0);
+
+	return true;
+}
+
+bool initDebugOutput()
+{
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB);
+	glDebugMessageControlARB(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+	glDebugMessageCallbackARB(&glf::debugOutput, NULL);
+
+	return true;
+}
+
+bool begin()
+{
+	bool Success(true);
+
+	// Validate OpenGL support
+	Success = Success && glf::checkGLVersion(SAMPLE_MAJOR_VERSION, SAMPLE_MINOR_VERSION);
+	Success = Success && glf::checkExtension("GL_ARB_multi_draw_indirect");
+
+	glGenQueries(1, &QueryName);
+
+#	if _DEBUG
+	if(Success && glf::checkExtension("GL_ARB_debug_output"))
+		Success = initDebugOutput();
+#	endif
+
+	if(Success)
+		Success = initProgram();
+	if(Success)
+		Success = initBuffer();
+	if(Success)
+		Success = initVertexArray();
+
+	return Success;
+}
+
+bool end()
+{
+	glDeleteBuffers(buffer::MAX, BufferName);
+	glDeleteProgramPipelines(1, &PipelineName);
+	glDeleteProgram(ProgramName);
+	glDeleteVertexArrays(1, &VertexArrayName);
+
+	return true;
+}
+
+void display()
+{
+	// Clear framebuffer
+	float Depth(1.0f);
+	glClearBufferfv(GL_DEPTH, 0, &Depth);
+	glClearBufferfv(GL_COLOR, 0, &glm::vec4(1.0f)[0]);
+
+	{
+		// Update the transformation matrix
+		glBindBuffer(GL_UNIFORM_BUFFER, BufferName[buffer::TRANSFORM]);
+		glm::mat4* Pointer = reinterpret_cast<glm::mat4*>(glMapBufferRange(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT));
+
+		glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 2048.0f);
+		glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Window.TranlationCurrent.y - 512));
+		glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Window.RotationCurrent.y, glm::vec3(1.f, 0.f, 0.f));
+		glm::mat4 View = glm::rotate(ViewRotateX, Window.RotationCurrent.x, glm::vec3(0.f, 1.f, 0.f));
+		glm::mat4 Model = glm::mat4(1.0f);
+
+		*Pointer = Projection * View * Model;
+		glUnmapBuffer(GL_UNIFORM_BUFFER);
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	glViewportIndexedfv(0, &glm::vec4(0, 0, Window.Size.x, Window.Size.y)[0]);
+
+	glBindBufferBase(GL_UNIFORM_BUFFER, glf::semantic::uniform::TRANSFORM0, BufferName[buffer::TRANSFORM]);
+	glBindProgramPipeline(PipelineName);
+	glBindVertexArray(VertexArrayName);
+	glBindBuffer(GL_DRAW_INDIRECT_BUFFER, BufferName[buffer::INDIRECT]);
+
+	glBeginQuery(GL_TIME_ELAPSED, QueryName);
+		glMultiDrawElementsIndirect(GL_TRIANGLES, GL_UNSIGNED_INT, 0, static_cast<GLsizei>(DrawCount), 0);
+	glEndQuery(GL_TIME_ELAPSED);
+
+	GLuint Time = 0;
+	glGetQueryObjectuiv(QueryName, GL_QUERY_RESULT, &Time);
+	fprintf(stdout, "Time: %f ms   \r", Time / 1000.f / 1000.f);
+
+	glf::swapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	return glf::run(
+		argc, argv,
+		glm::ivec2(::SAMPLE_SIZE_WIDTH, ::SAMPLE_SIZE_HEIGHT), 
+		GLF_CONTEXT_CORE_PROFILE_BIT, 
+		::SAMPLE_MAJOR_VERSION, 
+		::SAMPLE_MINOR_VERSION);
+}
