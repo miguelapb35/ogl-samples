47211e9362e5e0ec47549bb876a6f8df5aba7073
Christophe Riccio <christophe.riccio@unity3d.com>
2013-10-31 19:57:01 +0100

    Updated caps code, extension checks for queries

--- perfs/hz-430-thread.cpp
+++ perfs/hz-430-thread.cpp
@@ -277,6 +277,23 @@ namespace cmd
 		glm::vec4 const Color;
 		float const Depth;
 	};
+
+	struct buffer
+	{
+		enum flag
+		{
+			MAP_READ_BIT = (1 << 0),
+			MAP_WRITE_BIT = (1 << 1),
+			MAP_PERSISTENT_BIT = (1 << 2),
+			MAP_COHERENT_BIT = (1 << 3),
+			DYNAMIC_STORAGE_BIT = (1 << 4)
+		};
+
+		buffer(std::size_t const & Size, glm::uint const & flags);
+
+		GLsizeiptr const Size;
+		GLbitfield const Flags;
+	};
 }//cmd
 
 bool initProgram()
@@ -309,8 +326,8 @@ bool initBuffer()
 {
 	glGenBuffers(buffer::MAX, BufferName);
 
-	glNamedBufferDataEXT(BufferName[buffer::VERTEX], VertexSize, VertexData, GL_STATIC_DRAW);
-	glNamedBufferDataEXT(BufferName[buffer::TRANSFORM], sizeof(glm::mat4), NULL, GL_DYNAMIC_DRAW);
+	glNamedBufferStorageEXT(BufferName[buffer::VERTEX], VertexSize, VertexData, 0);
+	glNamedBufferStorageEXT(BufferName[buffer::TRANSFORM], sizeof(glm::mat4), NULL, GL_DYNAMIC_STORAGE_BIT);
 
 	std::vector<DrawArraysIndirectCommand> Commands;
 	Commands.resize(DrawCount);
