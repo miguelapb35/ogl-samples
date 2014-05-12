38558ae8322b2ab79fd485c5bc55ac7ce5f69282
Christophe Riccio <mail@g-truc.net>
2014-05-12 21:53:31 +0200

    Fixed sample, imcomplete texture

--- tests/gl-320-fbo-multisample-integer.cpp
+++ tests/gl-320-fbo-multisample-integer.cpp
@@ -216,6 +216,8 @@ private:
 		glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
 
 		glBindTexture(GL_TEXTURE_2D, TextureName[texture::COLORBUFFER]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8UI,
 			WindowSize.x / FRAMEBUFFER_SIZE,
 			WindowSize.y / FRAMEBUFFER_SIZE,
