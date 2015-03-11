56688f85f25e67e0e96307d05872c8274f8709f9
Christophe Riccio <mail@g-truc.net>
2015-03-11 02:02:00 +0100

    Completed 450-sample-location-nv

--- tests/gl-450-sample-location-nv.cpp
+++ tests/gl-450-sample-location-nv.cpp
@@ -69,6 +69,19 @@ namespace
 		};
 	}//namespace texture
 	
+	namespace framebuffer
+	{
+		enum type
+		{
+			COLORBUFFER,
+			RENDERBUFFER0,
+			RENDERBUFFER1,
+			RENDERBUFFER2,
+			RENDERBUFFER3,
+			MAX
+		};
+	}//namespace framebuffer
+
 	namespace program
 	{
 		enum type
@@ -108,7 +121,7 @@ private:
 	std::array<GLuint, buffer::MAX> BufferName;
 	std::array<GLuint, texture::MAX> TextureName;
 	std::array<GLint, program::MAX> UniformDiffuse;
-	std::array<GLuint, texture::MAX> FramebufferName;
+	std::array<GLuint, framebuffer::MAX> FramebufferName;
 	GLsizei VertexCount;
 	glm::uint FramebufferScale;
 	GLint UniformTransform;
@@ -218,8 +231,8 @@ private:
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, TextureName[texture::RENDERBUFFER]);
-		//glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_BASE_LEVEL, 0);
-		//glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_MAX_LEVEL, 0);
+		glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_BASE_LEVEL, 0);
+		glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_MAX_LEVEL, 0);
 		glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 8, GL_RGBA8, GLsizei(WindowSize.x), GLsizei(WindowSize.y), GL_TRUE);
 
 		return Validated;
@@ -246,21 +259,90 @@ private:
 
 	bool initFramebuffer()
 	{
-		static const glm::vec2 SamplesPositions[2][4] =
+		typedef std::array<glm::vec2, 8> sampleLocations;
+
+		static glm::vec2 SamplesPositions[][8] =
 		{
+			{
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f)
+			},
+			{
+				glm::vec2(0.0f / 7.0f),
+				glm::vec2(1.0f / 7.0f),
+				glm::vec2(2.0f / 7.0f),
+				glm::vec2(3.0f / 7.0f),
+				glm::vec2(4.0f / 7.0f),
+				glm::vec2(5.0f / 7.0f),
+				glm::vec2(6.0f / 7.0f),
+				glm::vec2(7.0f / 7.0f),
+			},
+			{
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f),
+				glm::vec2(0.00f, 0.00f)
+			},
 			{
 				glm::vec2(0.75f, 0.25f),
 				glm::vec2(1.00f, 0.75f),
 				glm::vec2(0.25f, 1.00f),
-				glm::vec2(0.00f, 0.25f)
-			},
-			{
+				glm::vec2(0.00f, 0.25f),
 				glm::vec2(0.25f, 0.25f),
 				glm::vec2(1.00f, 0.25f),
 				glm::vec2(0.75f, 1.00f),
 				glm::vec2(0.00f, 0.75f)
-			}
+			},
+			{
+				glm::vec2(0.50f, 0.50f),
+				glm::vec2(0.50f, 0.50f),
+				glm::vec2(0.50f, 0.50f),
+				glm::vec2(0.50f, 0.50f),
+				glm::vec2(0.50f, 0.50f),
+				glm::vec2(0.50f, 0.50f),
+				glm::vec2(0.50f, 0.50f),
+				glm::vec2(0.50f, 0.50f)
+			},
 		};
+
+		sampleLocations SampleLocationsRead;
+
+		glGenFramebuffers(framebuffer::MAX, &FramebufferName[0]);
+
+		for(int FramebufferIndex = 0; FramebufferIndex < 4; ++FramebufferIndex)
+		{
+			glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[framebuffer::RENDERBUFFER0 + FramebufferIndex]);
+			glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, TextureName[texture::RENDERBUFFER], 0);
+
+			GLint Samples = 0;
+			glGetIntegerv(GL_SAMPLES, &Samples);
+
+			if(FramebufferIndex == 0)
+			{
+				for(GLint i = 0; i < Samples; ++i)
+				{
+					glGetMultisamplefv(GL_SAMPLE_POSITION, i, &SamplesPositions[0][i][0]);
+					glGetMultisamplefv(GL_SAMPLE_POSITION, i, &SampleLocationsRead[i][0]);
+				}
+			}
+
+			glFramebufferParameteri(GL_FRAMEBUFFER, GL_FRAMEBUFFER_PROGRAMMABLE_SAMPLE_LOCATIONS_NV, GL_TRUE);
+			glFramebufferParameteri(GL_FRAMEBUFFER, GL_FRAMEBUFFER_SAMPLE_LOCATION_PIXEL_GRID_NV, GL_FALSE);
+			glFramebufferSampleLocationsfvNV(GL_FRAMEBUFFER, 0, Samples, &SamplesPositions[FramebufferIndex][0][0]);
+		}
+
+		for(std::size_t i = 0; i < SampleLocationsRead.size(); ++i)
+			SampleLocationsRead[i] *= 16.0f;
 
 		GLint SubPixelBits(0);
 		glm::ivec2 PixelGrid(0);
@@ -271,25 +353,12 @@ private:
 		glGetIntegerv(GL_SAMPLE_LOCATION_PIXEL_GRID_HEIGHT_NV, &PixelGrid.y);
 		glGetIntegerv(GL_PROGRAMMABLE_SAMPLE_LOCATION_TABLE_SIZE_NV, &TableSize);
 
-		std::vector<glm::vec2> SamplesLocations;
-		SamplesLocations.resize(PixelGrid.x * PixelGrid.y);
-		for (std::size_t i = 0; i < SamplesLocations.size(); ++i)
-			SamplesLocations[i] = glm::vec2(0.0f);
-
-		glGenFramebuffers(texture::MAX, &FramebufferName[0]);
-
-		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[texture::RENDERBUFFER]);
-		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, TextureName[texture::RENDERBUFFER], 0);
-		glFramebufferParameteri(GL_FRAMEBUFFER, GL_FRAMEBUFFER_PROGRAMMABLE_SAMPLE_LOCATIONS_NV, GL_TRUE);
-		glFramebufferSampleLocationsfvNV(GL_FRAMEBUFFER, 0, static_cast<GLsizei>(SamplesLocations.size()), &SamplesLocations[0][0]);
-
 		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[texture::COLORBUFFER]);
 		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, TextureName[texture::COLORBUFFER], 0);
 
-		if(this->checkFramebuffer(FramebufferName[texture::RENDERBUFFER]))
-			return false;
-		if(this->checkFramebuffer(FramebufferName[texture::COLORBUFFER]))
-			return false;
+		for(int FramebufferIndex = 0; FramebufferIndex < framebuffer::MAX; ++FramebufferIndex)
+			if(this->checkFramebuffer(FramebufferName[FramebufferIndex]))
+				return false;
 
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		return true;
@@ -346,8 +415,7 @@ private:
 			glUnmapBuffer(GL_UNIFORM_BUFFER);
 		}
 
-		glViewport(0, 0, static_cast<GLsizei>(WindowSize.x) >> this->FramebufferScale, static_cast<GLsizei>(WindowSize.y) >> this->FramebufferScale);
-		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[texture::RENDERBUFFER]);
+		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[framebuffer::RENDERBUFFER0]);
 		glClearBufferfv(GL_COLOR, 0, &glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)[0]);
 
 		glUseProgram(ProgramName[program::TEXTURE]);
@@ -355,16 +423,34 @@ private:
 		glBindVertexArray(VertexArrayName[program::TEXTURE]);
 		glBindBufferBase(GL_UNIFORM_BUFFER, semantic::uniform::TRANSFORM0, BufferName[buffer::TRANSFORM]);
 
+		glm::vec2 ViewportSize(glm::vec2(this->getWindowSize() >> this->FramebufferScale) * 0.5f);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[framebuffer::RENDERBUFFER0]);
+		glViewportIndexedf(0, 0, 0, ViewportSize.x, ViewportSize.y);
 		glDrawArraysInstanced(GL_LINE_LOOP, 0, this->VertexCount, 1);
 
-		glBindFramebuffer(GL_READ_FRAMEBUFFER, FramebufferName[texture::RENDERBUFFER]);
-		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, FramebufferName[texture::COLORBUFFER]);
+		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[framebuffer::RENDERBUFFER1]);
+		glViewportIndexedf(0, ViewportSize.x, 0, ViewportSize.x, ViewportSize.y);
+		glDrawArraysInstanced(GL_LINE_LOOP, 0, this->VertexCount, 1);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[framebuffer::RENDERBUFFER2]);
+		glViewportIndexedf(0, ViewportSize.x, ViewportSize.y, ViewportSize.x, ViewportSize.y);
+		glDrawArraysInstanced(GL_LINE_LOOP, 0, this->VertexCount, 1);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName[framebuffer::RENDERBUFFER3]);
+		glViewportIndexedf(0, 0, ViewportSize.y, ViewportSize.x, ViewportSize.y);
+		glDrawArraysInstanced(GL_LINE_LOOP, 0, this->VertexCount, 1);
+
+
+		// Blit
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, FramebufferName[framebuffer::RENDERBUFFER0]);
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, FramebufferName[framebuffer::COLORBUFFER]);
 		glBlitFramebuffer(
 			0, 0, static_cast<GLsizei>(WindowSize.x) >> this->FramebufferScale, static_cast<GLsizei>(WindowSize.y) >> this->FramebufferScale, 
 			0, 0, static_cast<GLsizei>(WindowSize.x) >> this->FramebufferScale, static_cast<GLsizei>(WindowSize.y) >> this->FramebufferScale, 
 			GL_COLOR_BUFFER_BIT, GL_NEAREST);
 
-		glViewport(0, 0, static_cast<GLsizei>(WindowSize.x), static_cast<GLsizei>(WindowSize.y));
+		glViewportIndexedf(0, 0, 0, WindowSize.x, WindowSize.y);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 		glUseProgram(ProgramName[program::SPLASH]);
