15308a4aab7bfe6d791530074b327fc35ee9e2f3
Christophe Riccio <mail@g-truc.net>
2015-03-25 22:22:50 +0100

    More use of DSA

--- tests/gl-500-buffer-sparse-arb.cpp
+++ tests/gl-500-buffer-sparse-arb.cpp
@@ -60,11 +60,11 @@ namespace
 	}//namespace buffer
 }//namespace
 
-class gl_500_buffer_storage : public test
+class gl_500_buffer_storage_arb : public test
 {
 public:
-	gl_500_buffer_storage(int argc, char* argv[]) :
-		test(argc, argv, "gl-500-buffer-storage-arb", test::CORE, 4, 3),
+	gl_500_buffer_storage_arb(int argc, char* argv[]) :
+		test(argc, argv, "gl-500-buffer-storage-arb", test::CORE, 4, 5),
 		PipelineName(0),
 		VertexArrayName(0),
 		TextureName(0),
@@ -87,8 +87,8 @@ private:
 		if(Validated)
 		{
 			compiler Compiler;
-			GLuint VertShaderName = Compiler.create(GL_VERTEX_SHADER, getDataDirectory() + VERT_SHADER_SOURCE, "--version 430 --profile core");
-			GLuint FragShaderName = Compiler.create(GL_FRAGMENT_SHADER, getDataDirectory() + FRAG_SHADER_SOURCE, "--version 430 --profile core");
+			GLuint VertShaderName = Compiler.create(GL_VERTEX_SHADER, getDataDirectory() + VERT_SHADER_SOURCE, "--version 450 --profile core");
+			GLuint FragShaderName = Compiler.create(GL_FRAGMENT_SHADER, getDataDirectory() + FRAG_SHADER_SOURCE, "--version 450 --profile core");
 			Validated = Validated && Compiler.check();
 
 			ProgramName = glCreateProgram();
@@ -101,7 +101,7 @@ private:
 
 		if(Validated)
 		{
-			glGenProgramPipelines(1, &PipelineName);
+			glCreateProgramPipelines(1, &PipelineName);
 			glUseProgramStages(PipelineName, GL_VERTEX_SHADER_BIT | GL_FRAGMENT_SHADER_BIT, ProgramName);
 		}
 
@@ -116,17 +116,17 @@ private:
 
 		bool Validated(true);
 
-		glGenBuffers(buffer::MAX, &BufferName[0]);
-
 		GLintptr CopyBufferSize = glm::higherMultiple<GLint>(VertexSize, Alignement) + glm::higherMultiple<GLint>(ElementSize, Alignement);
 
-		glBindBuffer(GL_COPY_READ_BUFFER, BufferName[buffer::COPY]);
-		glBufferStorage(GL_COPY_READ_BUFFER, CopyBufferSize, nullptr, GL_MAP_WRITE_BIT);
+		glCreateBuffers(buffer::MAX, &BufferName[0]);
 
-		glm::byte* CopyBufferPointer = reinterpret_cast<glm::byte*>(glMapBufferRange(GL_COPY_READ_BUFFER, 0, CopyBufferSize, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT));
+		glNamedBufferStorage(BufferName[buffer::COPY], CopyBufferSize, nullptr, GL_MAP_WRITE_BIT);
+		glm::byte* CopyBufferPointer = reinterpret_cast<glm::byte*>(glMapNamedBufferRange(BufferName[buffer::COPY], 0, CopyBufferSize, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT));
 		memcpy(CopyBufferPointer + 0, VertexData, VertexSize);
 		memcpy(CopyBufferPointer + glm::higherMultiple<GLint>(VertexSize, Alignement), ElementData, ElementSize);
-		glUnmapBuffer(GL_COPY_READ_BUFFER);
+		glUnmapNamedBuffer(BufferName[buffer::COPY]);
+
+		glBindBuffer(GL_COPY_READ_BUFFER, BufferName[buffer::COPY]);
 
 		glBindBuffer(GL_COPY_WRITE_BUFFER, BufferName[buffer::ELEMENT]);
 		glBufferStorage(GL_COPY_WRITE_BUFFER, glm::higherMultiple<GLint>(ElementSize, BufferPageSize), nullptr, GL_SPARSE_STORAGE_BIT_ARB);
@@ -142,9 +142,7 @@ private:
 		glGetIntegerv(GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT, &UniformBufferOffset);
 		GLint UniformBlockSize = glm::max(GLint(sizeof(glm::mat4)), UniformBufferOffset);
 
-		glBindBuffer(GL_UNIFORM_BUFFER, BufferName[buffer::TRANSFORM]);
-		glBufferStorage(GL_UNIFORM_BUFFER, UniformBlockSize, nullptr, GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
-		glBindBuffer(GL_UNIFORM_BUFFER, 0);
+		glNamedBufferStorage(BufferName[buffer::TRANSFORM], UniformBlockSize, nullptr, GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT);
 
 		return Validated;
 	}
@@ -157,36 +155,30 @@ private:
 
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-		glGenTextures(1, &TextureName);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D_ARRAY, TextureName);
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_SWIZZLE_R, GL_RED);
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_SWIZZLE_G, GL_GREEN);
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_SWIZZLE_B, GL_BLUE);
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_SWIZZLE_A, GL_ALPHA);
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_BASE_LEVEL, 0);
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAX_LEVEL, GLint(Texture.levels() - 1));
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glCreateTextures(GL_TEXTURE_2D_ARRAY, 1, &TextureName);
+		glTextureParameteri(TextureName, GL_TEXTURE_SWIZZLE_R, GL_RED);
+		glTextureParameteri(TextureName, GL_TEXTURE_SWIZZLE_G, GL_GREEN);
+		glTextureParameteri(TextureName, GL_TEXTURE_SWIZZLE_B, GL_BLUE);
+		glTextureParameteri(TextureName, GL_TEXTURE_SWIZZLE_A, GL_ALPHA);
+		glTextureParameteri(TextureName, GL_TEXTURE_BASE_LEVEL, 0);
+		glTextureParameteri(TextureName, GL_TEXTURE_MAX_LEVEL, static_cast<GLint>(Texture.levels() - 1));
+		glTextureParameteri(TextureName, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTextureParameteri(TextureName, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-		glTexStorage3D(GL_TEXTURE_2D_ARRAY, GLint(Texture.levels()),
+		glTextureStorage3D(TextureName, static_cast<GLint>(Texture.levels()),
 			gli::internal_format(Texture.format()),
-			GLsizei(Texture[0].dimensions().x), GLsizei(Texture[0].dimensions().y), GLsizei(1));
+			static_cast<GLsizei>(Texture[0].dimensions().x), static_cast<GLsizei>(Texture[0].dimensions().y), static_cast<GLsizei>(1));
 
 		for(gli::texture2D::size_type Level = 0; Level < Texture.levels(); ++Level)
 		{
-			glTexSubImage3D(GL_TEXTURE_2D_ARRAY, GLint(Level),
+			glTextureSubImage3D(TextureName, static_cast<GLint>(Level),
 				0, 0, 0,
-				GLsizei(Texture[Level].dimensions().x),
-				GLsizei(Texture[Level].dimensions().y),
-				GLsizei(1),
+				static_cast<GLsizei>(Texture[Level].dimensions().x), static_cast<GLsizei>(Texture[Level].dimensions().y), static_cast<GLsizei>(1),
 				gli::external_format(Texture.format()),
 				gli::type_format(Texture.format()),
 				Texture[Level].data());
 		}
 	
-		glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-
 		return Validated;
 	}
 
@@ -194,10 +186,8 @@ private:
 	{
 		bool Validated(true);
 
-		glGenVertexArrays(1, &VertexArrayName);
-		glBindVertexArray(VertexArrayName);
-			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, BufferName[buffer::ELEMENT]);
-		glBindVertexArray(0);
+		glCreateVertexArrays(1, &VertexArrayName);
+		glVertexArrayElementBuffer(VertexArrayName, BufferName[buffer::ELEMENT]);
 
 		return Validated;
 	}
@@ -218,9 +208,8 @@ private:
 			Validated = initTexture();
 		if(Validated)
 		{
-			glBindBuffer(GL_UNIFORM_BUFFER, BufferName[buffer::TRANSFORM]);
-			UniformPointer = (glm::mat4*)glMapBufferRange(
-				GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4),
+			UniformPointer = (glm::mat4*)glMapNamedBufferRange(
+				BufferName[buffer::TRANSFORM], 0, sizeof(glm::mat4),
 				GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
 		}
 
@@ -231,8 +220,7 @@ private:
 	{
 		if(!UniformPointer)
 		{
-			glBindBuffer(GL_UNIFORM_BUFFER, BufferName[buffer::TRANSFORM]);
-			glUnmapBuffer(GL_UNIFORM_BUFFER);
+			glUnmapNamedBuffer(BufferName[buffer::TRANSFORM]);
 			UniformPointer = nullptr;
 		}
 
@@ -260,8 +248,7 @@ private:
 		glClearBufferfv(GL_COLOR, 0, &glm::vec4(1.0f)[0]);
 
 		glBindProgramPipeline(PipelineName);
-		glActiveTexture(GL_TEXTURE0 + semantic::sampler::DIFFUSE);
-		glBindTexture(GL_TEXTURE_2D_ARRAY, TextureName);
+		glBindTextureUnit(semantic::sampler::DIFFUSE, TextureName);
 		glBindVertexArray(VertexArrayName);
 		glBindBufferBase(GL_UNIFORM_BUFFER, semantic::uniform::TRANSFORM0, BufferName[buffer::TRANSFORM]);
 		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, semantic::storage::VERTEX, BufferName[buffer::VERTEX]);
@@ -276,7 +263,7 @@ int main(int argc, char* argv[])
 {
 	int Error(0);
 
-	gl_500_buffer_storage Test(argc, argv);
+	gl_500_buffer_storage_arb Test(argc, argv);
 	Error += Test();
 
 	return Error;
