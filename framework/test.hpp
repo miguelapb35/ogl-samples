da5ef7c5d67404eb009bdcea67a05a571b03cfad
Christophe Riccio <christopheri@unity3d.com>
2015-09-08 21:38:08 +0200

    Fixed build

--- framework/test.hpp
+++ framework/test.hpp
@@ -41,7 +41,7 @@
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
-#include <glm/gtc/color.hpp>
+#include <glm/gtc/color_space.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/packing.hpp>
 #include <glm/gtc/random.hpp>
